83d860c96a43a5d436d420aefa5310c3df913297
Nikita Savin <klaster26@mail.ru>
2023-10-19 22:50:59 +0300

    feat: solve task 'Towel Sort'

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let array = [];
+  if (matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 == 0) {
+        array.push(matrix[i]);        
+      } else {
+        array.push(matrix[i].reverse());
+      }
+    } 
+    return array.reduce ((result, arr) => {
+      return result.concat(arr);
+      }, []);
+    } return array;
+  }
